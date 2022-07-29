b056b0e6da3f3f3544b86fe4a738f51c886d68d1
karkir0003 <karkir0003@gmail.com>
2022-07-29 16:50:08 -0700

    dockerfile attach

--- src/course_inviter.py
+++ src/course_inviter.py
@@ -3,15 +3,3 @@
 # can't be invited. Parse that error message and autosend email notification from dsgtbootcamp@gmail.com with a message saying to 
 # provide proper udemy account credentials
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-user_name = "YOUR EMAILID"
-password = "YOUR PASSWORD"
-driver = webdriver.Firefox(executable_path=r'C:\Users\karki\geckodriver-v0.30.0-win64\geckodriver.exe')
-driver.get("https://www.facebook.com")
-element = driver.find_element_by_id("email")
-element.send_keys(user_name)
-element = driver.find_element_by_id("pass")
-element.send_keys(password)
-element.send_keys(Keys.RETURN)
-element.close()
